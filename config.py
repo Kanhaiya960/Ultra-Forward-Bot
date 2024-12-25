886c0f97ac2e269e81eec430e27283d952c87b9d
Kanhaiya960 <168913838+Kanhaiya960@users.noreply.github.com>
2024-12-25 22:57:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config:
     DATABASE_NAME = environ.get("DATABASE_NAME", "Kanhaiya2")
     BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '6552970915').split()]
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002237007652'))
-    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "-1002025958156") 
+    FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.me/Kanus_Network") 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
     PORT = environ.get('PORT', '8080')
     
